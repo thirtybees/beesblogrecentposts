061b39565cabb30db81f39aef51d1212763c7503
Petr Hučík <petr@getdatakick.com>
2019-11-29 11:40:20 +0100

    version 1.0.1

--- beesblogrecentposts.php
+++ beesblogrecentposts.php
@@ -35,7 +35,7 @@ class BeesBlogRecentPosts extends Module
     {
         $this->name = 'beesblogrecentposts';
         $this->tab = 'front_office_features';
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->author = 'thirty bees';
 
         $this->bootstrap = true;
